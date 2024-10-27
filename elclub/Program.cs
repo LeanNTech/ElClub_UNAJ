321a42ac33676c09755cce9f5af938198354d389
Brian Retamal <95369610+LeanNTech@users.noreply.github.com>
2024-10-27 14:28:48 -0300

    App_Base
    App inicial

---
+++ elclub/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Brian
+ * Date: 27/10/2024
+ * Time: 14:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace elclub
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
