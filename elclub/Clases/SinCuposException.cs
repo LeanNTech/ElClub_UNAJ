340c383574a56458c7bb1d4147b4884ca47b38b7
Javier Florentin <javier.florentin.pri@gmail.com>
2024-11-17 00:46:50 -0300

    Version 3
    Se completaron todas las funciones del menú, pero quedan algunos pocos errores que corregir

---
+++ elclub/Clases/SinCuposException.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Usuario
+ * Fecha: 17/11/2024
+ * Hora: 00:42
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace elclub.Clases
+{
+	/// <summary>
+	/// Description of SinCuposException.
+	/// </summary>
+	public class SinCuposException
+	{
+		public SinCuposException()
+		{
+		}
+	}
+}
